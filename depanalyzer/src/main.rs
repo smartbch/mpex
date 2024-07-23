31b8541275791b5484a0f4978a5be4cac38c1e06
mpex-dev <mpex-dev@proton.me>
2024-07-23 09:50:04 +0800

    refine depanalyze

--- depanalyzer/src/main.rs
+++ depanalyzer/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::{
     fs::{File, OpenOptions},
@@ -8,18 +9,20 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use serde::Deserialize;
 use exepipe::exetask::AccessSet;
-use exepipe::scheduler::{ParaBloom, MAX_TASKS_LEN_IN_BUNDLE, SET_MAX_SIZE};
+use exepipe::scheduler::{PBElement, ParaBloom, MAX_TASKS_LEN_IN_BUNDLE, SET_MAX_SIZE};
 use mpads::utils::hasher;
 use byteorder::{BigEndian, ByteOrder};
 
+const WETH_ADDR: &str = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+//const WETH_ADDR: &str = "";
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct Slot {
     addr: String,
     index: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct Tx {
     txid: String,
     rdo_addr_list: Vec<String>,
@@ -29,35 +32,34 @@ struct Tx {
 }
 
 impl Tx {
-    fn to_access_set(&self) -> AccessSet {
+    fn to_access_set(&self, coinbase: &str) -> AccessSet {
         let mut access_set = AccessSet::new();
         for rdo_addr in self.rdo_addr_list.iter() {
             let hash = hasher::hash(rdo_addr);
             access_set.rdo_k64_vec.push(BigEndian::read_u64(&hash[..8]));
+            access_set.rdo_set.insert(hash);
         }
         for rnw_addr in self.rnw_addr_list.iter() {
-            if rnw_addr == "0x95222290dd7278aa3ddd389cc1e1d165cc4bafe5" {
-                println!("ignore beaverbuild");
-                continue; //ignore beaverbuild
+            if rnw_addr == coinbase {
+                continue;
             }
-            if rnw_addr == "0x4838b106fce9647bdf1e7877bf73ce8b0bad5f97" {
-                println!("ignore Titan Build");
-                continue; //ignore Titan Build
-            }
-            if rnw_addr == "0x7e2a2fa2a064f693f0a55c5639476d913ff12d05" {
-                continue; //MEV builder
+            if rnw_addr == WETH_ADDR {
+                continue;
             }
             let hash = hasher::hash(rnw_addr);
             access_set.rnw_k64_vec.push(BigEndian::read_u64(&hash[..8]));
+            access_set.rnw_set.insert(hash);
         }
         let mut buf = [0u8; 52];
         for rdo_slot in self.rdo_slot_list.iter() {
             let hash = hasher::hash("".to_owned()+&rdo_slot.addr+&rdo_slot.index);
             access_set.rdo_k64_vec.push(BigEndian::read_u64(&hash[..8]));
+            access_set.rdo_set.insert(hash);
         }
         for rnw_slot in self.rnw_slot_list.iter() {
             let hash = hasher::hash("".to_owned()+&rnw_slot.addr+&rnw_slot.index);
             access_set.rnw_k64_vec.push(BigEndian::read_u64(&hash[..8]));
+            access_set.rnw_set.insert(hash);
         }
         access_set
     }
@@ -67,6 +69,7 @@ impl Tx {
 struct Block {
     height: i64,
     tx_list: Vec<Tx>,
+    coinbase: String,
 }
 
 #[derive(Deserialize, Debug)]
@@ -75,11 +78,120 @@ struct User {
     location: String,
 }
 
-pub const BUNDLE_COUNT: usize = 128;
+struct AccessSetList<T: PBElement> {
+    rdo_set_list: Vec<HashSet<[u8; 32]>>,
+    rnw_set_list: Vec<HashSet<[u8; 32]>>,
+    _zero: T,
+}
+
+impl<T: PBElement> AccessSetList<T> {
+    pub fn new() -> Self {
+        let len = T::BITS as usize;
+        let mut res = Self {
+            rdo_set_list: Vec::with_capacity(len),
+            rnw_set_list: Vec::with_capacity(len),
+            _zero: T::zero(),
+        };
+        for _ in 0..len {
+            res.rdo_set_list.push(HashSet::new());
+            res.rnw_set_list.push(HashSet::new());
+        }
+        res
+    }
+
+    pub fn get_rdo_set_size(&self, id: usize) -> usize {
+        return self.rdo_set_list[id].len();
+    }
+
+    pub fn get_rnw_set_size(&self, id: usize) -> usize {
+        return self.rnw_set_list[id].len();
+    }
+
+
+    //pub rdo_set: HashSet<[u8; 32]>,
+    //pub rnw_set: HashSet<[u8; 32]>,
+
+    pub fn get_dep_mask(&self, access_set: &AccessSet) -> T {
+        let mut result = T::zero();
+        let mut mask = T::one();
+        for idx in 0..(T::BITS as usize) {
+            for hash in access_set.rdo_set.iter() {
+                if self.rnw_set_list[idx].contains(hash) {
+                    result = result | mask;
+                }
+            }
+            for hash in access_set.rnw_set.iter() {
+                if self.rdo_set_list[idx].contains(hash) {
+                    result = result | mask;
+                }
+                if self.rnw_set_list[idx].contains(hash) {
+                    result = result | mask;
+                }
+            }
+            mask = mask << 1usize;
+        }
+        result
+    }
+
+    pub fn clear(&mut self, idx: usize) {
+        self.rdo_set_list[idx].clear();
+        self.rnw_set_list[idx].clear();
+    }
+
+    pub fn clear_all(&mut self) {
+        for idx in 0..(T::BITS as usize) {
+            self.rdo_set_list[idx].clear();
+            self.rnw_set_list[idx].clear();
+        }
+    }
+
+
+    pub fn add(&mut self, idx: usize, access_set: &AccessSet) {
+        for &hash in access_set.rdo_set.iter() {
+            self.rdo_set_list[idx].insert(hash);
+        }
+        for &hash in access_set.rnw_set.iter() {
+            self.rnw_set_list[idx].insert(hash);
+        }
+    }
+}
+
+fn merge_access_set(a: &mut AccessSet, b: &AccessSet, only_rnw: bool) {
+    for &hash in b.rdo_set.iter() {
+        if only_rnw {
+            a.rnw_set.insert(hash);
+        } else {
+            a.rdo_set.insert(hash);
+        }
+    }
+    for &hash in b.rnw_set.iter() {
+        a.rnw_set.insert(hash);
+    }
+}
+
+fn access_set_conflicts(a: &AccessSet, b: &AccessSet) -> bool {
+    for hash in b.rdo_set.iter() {
+        if a.rnw_set.contains(hash) {
+            return true;
+        }
+    }
+    for hash in b.rnw_set.iter() {
+        if a.rdo_set.contains(hash) {
+            return true;
+        }
+        if a.rnw_set.contains(hash) {
+            return true;
+        }
+    }
+    false
+}
+
+pub const BUNDLE_COUNT: usize = 64;
 
 struct Scheduler {
-    pb: ParaBloom<u128>,
-    bundles: Vec<VecDeque<String>>,
+    pb: ParaBloom<u64>,
+    //pb: AccessSetList<u128>,
+    bundles: Vec<VecDeque<(String, AccessSet)>>,
 }
 
 impl Scheduler {
@@ -90,6 +202,7 @@ impl Scheduler {
         }
         Scheduler {
             pb: ParaBloom::new(),
+            //pb: AccessSetList::new(),
             bundles,
         }
     }
@@ -106,13 +219,25 @@ impl Scheduler {
         largest_id
     }
 
-    fn flush_all(&mut self) {
+    fn flush_all(&mut self, total_bundle: &mut usize) {
         println!("AA flush_all");
-        for i in 0..BUNDLE_COUNT {
-            if self.bundles[i].len() == 0 {
-                continue;
+        let mut bundle_set = AccessSet::new();
+        let mut bundle_size = 0;
+        for bundle_id in 0..BUNDLE_COUNT {
+            let target = self.bundles.get_mut(bundle_id).unwrap();
+            while target.len() != 0 {
+                let (_txid, access_set) = target.pop_front().unwrap();
+                if access_set_conflicts(&bundle_set, &access_set) {
+                    println!("BundleSize {}", bundle_size);
+                    bundle_set.rdo_set.clear();
+                    bundle_set.rnw_set.clear();
+                    bundle_size = 0;
+                    *total_bundle += 1;
+                } else {
+                    merge_access_set(&mut bundle_set, &access_set, false);
+                    bundle_size += 1;
+                }
             }
-            self.flush_bundle(i);
         }
     }
 
@@ -120,14 +245,14 @@ impl Scheduler {
         let target = self.bundles.get_mut(bundle_id).unwrap();
         println!("AA BeginBundle size={}", target.len()); 
         while target.len() != 0 {
-            let txid = target.pop_front().unwrap();
+            let (txid, _access_set) = target.pop_front().unwrap();
             println!("{:?}", txid);
         }
         println!("EndBundle"); 
     }
 
-    fn add_tx(&mut self, tx: &Tx) {
-        let access_set = tx.to_access_set();
+    fn add_tx(&mut self, tx: &Tx, coinbase: &str, total_bundle: &mut usize) {
+        let access_set = tx.to_access_set(coinbase);
         let mask = self.pb.get_dep_mask(&access_set);
         println!("depmask={:#016x}", mask);
         let mut bundle_id = mask.trailing_ones() as usize;
@@ -137,13 +262,14 @@ impl Scheduler {
             bundle_id = self.largest_bundle();
             self.pb.clear(bundle_id);
             self.flush_bundle(bundle_id);
+            *total_bundle += 1;
         }
 
         // now the task can be inserted into a bundle
         self.pb.add(bundle_id, &access_set);
 
         let target = self.bundles.get_mut(bundle_id).unwrap();
-        target.push_back("".to_owned()+&tx.txid);
+        target.push_back((tx.txid.clone(), access_set));
 
         // flush the bundle if it's large enough
         if self.pb.get_rdo_set_size(bundle_id) > SET_MAX_SIZE
@@ -152,6 +278,37 @@ impl Scheduler {
         {
             self.pb.clear(bundle_id);
             self.flush_bundle(bundle_id);
+            *total_bundle += 1;
+        }
+    }
+}
+
+struct SerialIssuer {
+    bundle_set: AccessSet,
+    bundle_size: usize,
+    only_rnw: bool
+}
+
+impl SerialIssuer {
+    fn new(only_rnw: bool) -> Self {
+        Self {
+            bundle_set: AccessSet::new(),
+            bundle_size: 0,
+            only_rnw,
+        }
+    }
+
+    fn add_tx(&mut self, tx: &Tx, coinbase: &str, total_bundle: &mut usize) {
+        let access_set = tx.to_access_set(coinbase);
+        if access_set_conflicts(&self.bundle_set, &access_set) {
+            println!("BundleSize {}", self.bundle_size);
+            self.bundle_set.rdo_set.clear();
+            self.bundle_set.rnw_set.clear();
+            self.bundle_size = 0;
+            *total_bundle += 1;
+        } else {
+            merge_access_set(&mut self.bundle_set, &access_set, self.only_rnw);
+            self.bundle_size += 1;
         }
     }
 
@@ -183,24 +340,65 @@ where P: AsRef<Path>, {
 }
 
 fn main() {
-    let mut scheduler = Scheduler::new();
-    let mut file = OpenOptions::new()
-        .read(true)
-        .open("blocks.json")
-        .expect("Could not read file");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    
-    let blocks: Vec<Block> = serde_json::from_str(&contents).unwrap();
+    run_scheduler();
+    //run_serial_issuer();
+}
 
-    //count_addr(&blocks);
- 
-    for blk in blocks.iter() {
-        for tx in blk.tx_list.iter() {
-            scheduler.add_tx(tx);
+fn run_serial_issuer() {
+    let mut serial_issuer = SerialIssuer::new(false);
+    let mut total_tx = 0;
+    let mut total_bundle = 0;
+    for id in (20338810..20338850).step_by(10) {
+        let mut file = OpenOptions::new()
+            .read(true)
+            .open(format!("blocks/blocks_{}.json", id))
+            .expect("Could not read file");
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        
+        let blocks: Vec<Block> = serde_json::from_str(&contents).unwrap();
+
+        for blk in blocks.iter() {
+            let coinbase = blk.coinbase.to_lowercase();
+            for tx in blk.tx_list.iter() {
+                total_tx += 1;
+                serial_issuer.add_tx(tx, &coinbase, &mut total_bundle);
+            }
         }
     }
-    scheduler.flush_all();
+    if serial_issuer.bundle_size != 0 {
+        total_bundle += 1;
+    }
+    println!("total_tx={} total_bundle={}", total_tx, total_bundle);
+}
+
+fn run_scheduler() {
+    let mut scheduler = Scheduler::new();
+    let mut total_bundle = 0usize;
+    let mut total_tx = 0usize;
+    for id in (20338810..20338850).step_by(10) {
+        let mut file = OpenOptions::new()
+            .read(true)
+            .open(format!("blocks/blocks_{}.json", id))
+            .expect("Could not read file");
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        
+        let blocks: Vec<Block> = serde_json::from_str(&contents).unwrap();
+
+        //count_addr(&blocks);
+     
+        for blk in blocks.iter() {
+            let coinbase = blk.coinbase.to_lowercase();
+            for tx in blk.tx_list.iter() {
+                total_tx += 1;
+                scheduler.add_tx(tx, &coinbase, &mut total_bundle);
+            }
+        }
+    }
+    println!("AA total_tx={}", total_tx);
+    scheduler.flush_all(&mut total_bundle);
+    println!("AA total_bundle={}", total_bundle); 
 
     //let lines = read_lines("./blocks.txt").unwrap();
     //for line in lines.flatten() {
